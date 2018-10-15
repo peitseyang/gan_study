f31186df64d41fa79b163d8ba34e3ff2b573d08d
AlexYang <alex10130010@gmail.com>
2018-10-16 00:49:20 +0800

    dataloader for celebA

--- src/gan/main.py
+++ src/gan/main.py
@@ -3,36 +3,171 @@ import torch
 import torch.nn as nn
 from torchvision import datasets
 import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import numpy as np
 from torch.autograd import Variable
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--epochs', type=int, default=200)
+parser.add_argument('--data_root', type=str, default='../../data/celebA/img_align_celeba')
+parser.add_argument('--workers', type=int, default=1)
 parser.add_argument('--batch_size', type=int, default=64)
-parser.add_argument('--lr', type=float, default=0.0001)
-parser.add_argument('--momentum', type=float, default=0.5)
+parser.add_argument('--img_size', type=int, default=64, help='size of each image dimension')
+parser.add_argument('--channels', type=int, default=3, help='number of image channels')
+parser.add_argument('--latent_dim', type=int, default=100, help='dimensionality of the latent space')
+# (?) ngf - relates to the depth of feature maps carried through the generator
+# (?) ndf - sets the depth of feature maps propagated through the discriminator
+parser.add_argument('--epochs', type=int, default=200)
+parser.add_argument('--lr', type=float, default=0.0002)
+parser.add_argument('--b1', type=float, default=0.5)
+parser.add_argument('--b2', type=float, default=0.999)
+# (?) ngpu - number of GPUs available. If this is 0, code will run in CPU mode. If this number is greater than 0 it will run on that number of GPUs
 # parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
-# parser.add_argument('--latent_dim', type=int, default=100, help='dimensionality of the latent space')
-# parser.add_argument('--img_size', type=int, default=28, help='size of each image dimension')
-# parser.add_argument('--channels', type=int, default=1, help='number of image channels')
 # parser.add_argument('--sample_interval', type=int, default=400, help='interval betwen image samples')
 opt = parser.parse_args()
 print(opt)
 
-dataloader = torch.utils.data.DataLoader(
-    datasets.MNIST('../../data/mnist', train=True, download=True,
-                   transform=transforms.Compose([
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-                   ])),
-    batch_size=opt.batch_size, shuffle=True)
-print(dataloader)
+dataset = datasets.ImageFolder(root=opt.data_root,
+                                transform=transforms.Compose([
+                                    transforms.Resize(opt.img_size),
+                                    transforms.CenterCrop(opt.img_size),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                                ]))
 
-# class Generator(nn.Module):
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batch_size,
+                                        shuffle=True, num_workers=opt.workers)
 
-# class Discriminator(nn.Module):
+print_batch = next(iter(dataloader))
+plt.figure(figsize=(8,8))
+plt.axis("off")
+plt.title("Images")
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to("cpu")[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+
+        def block(in_feat, out_feat, normalize=True):
+            layers = [nn.Linear(in_feat, out_feat)]
+            if normalize:
+                layers.append(nn.BatchNorm1d(out_feat, 0.8))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *block(opt.latent_dim, 128, normalize=False),
+            *block(128, 256),
+            *block(256, 512),
+            *block(512, 1024),
+            nn.Linear(1024, int(np.prod(img_shape))),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        img = self.model(z)
+        img = img.view(img.size(0), *img_shape)
+        return img
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(int(np.prod(img_shape)), 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, img):
+        img_flat = img.view(img.size(0), -1)
+        validity = self.model(img_flat)
+
+        return validity
 
 generator = Generator().cuda()
 discriminator = Discriminator().cuda()
 loss = nn.CrossEntropyLoss().cuda() # (?) BCELoss()
-generator_Optimizer = torch.optim.Adam(generator.parameters(), lr=opt.lr)
-discriminator_Optimizer = torch.optim.Adam(discriminator.parameters(), lr=opt.lr)
+generator_Optimizer = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+discriminator_Optimizer = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+
+# for epoch in range(opt.n_epochs):
+#     for i, data in enumerate(dataloader, 0):
+
+
+
+
+# for epoch in range(num_epochs):
+#     # For each batch in the dataloader
+#     for i, data in enumerate(dataloader, 0):
+
+#         ############################
+#         # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+#         ###########################
+#         ## Train with all-real batch
+#         netD.zero_grad()
+#         # Format batch
+#         real_cpu = data[0].to(device)
+#         b_size = real_cpu.size(0)
+#         label = torch.full((b_size,), real_label, device=device)
+#         # Forward pass real batch through D
+#         output = netD(real_cpu).view(-1)
+#         # Calculate loss on all-real batch
+#         errD_real = criterion(output, label)
+#         # Calculate gradients for D in backward pass
+#         errD_real.backward()
+#         D_x = output.mean().item()
+
+#         ## Train with all-fake batch
+#         # Generate batch of latent vectors
+#         noise = torch.randn(b_size, nz, 1, 1, device=device)
+#         # Generate fake image batch with G
+#         fake = netG(noise)
+#         label.fill_(fake_label)
+#         # Classify all fake batch with D
+#         output = netD(fake.detach()).view(-1)
+#         # Calculate D's loss on the all-fake batch
+#         errD_fake = criterion(output, label)
+#         # Calculate the gradients for this batch
+#         errD_fake.backward()
+#         D_G_z1 = output.mean().item()
+#         # Add the gradients from the all-real and all-fake batches
+#         errD = errD_real + errD_fake
+#         # Update D
+#         optimizerD.step()
+
+#         ############################
+#         # (2) Update G network: maximize log(D(G(z)))
+#         ###########################
+#         netG.zero_grad()
+#         label.fill_(real_label)  # fake labels are real for generator cost
+#         # Since we just updated D, perform another forward pass of all-fake batch through D
+#         output = netD(fake).view(-1)
+#         # Calculate G's loss based on this output
+#         errG = criterion(output, label)
+#         # Calculate gradients for G
+#         errG.backward()
+#         D_G_z2 = output.mean().item()
+#         # Update G
+#         optimizerG.step()
+
+#         # Output training stats
+#         if i % 50 == 0:
+#             print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+#                   % (epoch, num_epochs, i, len(dataloader),
+#                      errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+
+#         # Save Losses for plotting later
+#         G_losses.append(errG.item())
+#         D_losses.append(errD.item())
+
+#         # Check how the generator is doing by saving G's output on fixed_noise
+#         if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
+#             with torch.no_grad():
+#                 fake = netG(fixed_noise).detach().cpu()
+#             img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+
+#         iters += 1
